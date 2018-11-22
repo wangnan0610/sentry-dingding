28f6e6588aca40e837417f4453f66655f46e3871
wangnan0610 <375584419@qq.com>
2018-11-22 14:38:16 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sentry-dingding
 sentry的钉钉插件
+
+## 项目中要在sentry里面接入钉钉通知，但是现成资源里面https://github.com/L3T/sentry-dingding作者和地址有些问题，同时功能上不是特别match，所以新建一个项目做些小修改，满足需求。
+
+
---
+++ sentry_dingding/models.py
@@ -0,0 +1,57 @@
+import requests
+from django import forms
+from sentry.plugins.bases.notify import NotifyPlugin
+import sentry_dingding
+
+class DingDingOptionsForm(forms.Form):
+    webhook = forms.CharField(help_text="DingDing Webhook", required=True,
+                               widget=forms.TextInput(attrs={'placeholder': 'dingding webhook'}))
+
+class DingDingMessage(NotifyPlugin):
+    author = 'Wangnan0610'
+    author_url = 'https://github.com/wangnan0610/sentry-dingding'
+    version = sentry_dingding.VERSION
+    description = "钉钉通知"
+    resource_links = [
+        ('Bug Tracker', 'https://github.com/wangnan0610/sentry-dingding/issues'),
+        ('Source', 'https://github.com/wangnan0610/sentry-dingding'),
+    ]
+    slug = 'dingding'
+    title = 'DingDing'
+    conf_title = title
+    conf_key = 'dingding'
+    project_conf_form = DingDingOptionsForm
+
+    def is_configured(self, project):
+        return bool(self.get_option('webhook', project))
+
+    def notify_users(self, group, event, fail_silently=False):
+        project = event.project
+        link = group.get_absolute_url()
+        webhook = self.get_option('webhook', project)
+        try:
+            exception = event.get_interfaces()['sentry.interfaces.Exception'].to_string(event)
+            msg = exception.replace('  ', '&emsp;').replace('\n', '</br>')
+        except KeyError:
+            msg = event.error()
+        data = {
+            "msgtype": "markdown",
+            "markdown": {
+                "text": '''### 项目{project_name}发生错误，请尽快查看处理!   \n  ![screenshot](http://pic1.zhimg.com/v2-c41cd2b3b6c58245908ad89f7ddc55e7_b.jpg)
+> [点我直接查看BUG]({link})
+                '''.format(
+                    project_name=project,
+                    link=link,
+                ),
+            },
+        }
+        self.send_payload(
+            webhook=webhook,
+            data=data
+        )
+
+    def send_payload(self, webhook, data):
+        requests.post(
+            webhook,
+            json=data,
+        )
